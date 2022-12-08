b5d58ad803a045fa7a29f796d25344154634e64b
DarrenRobertLowe <61787379+DarrenRobertLowe@users.noreply.github.com>
2022-12-08 16:13:33 +0000

    Backing Up Database Again
    This time with a create-schema option included.

--- Dump20221208_includes_create_schema.sql
+++ Dump20221208_includes_create_schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `webstoredb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `webstoredb`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: webstoredb
@@ -445,4 +447,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-08 16:12:27
+-- Dump completed on 2022-12-08 16:12:47
